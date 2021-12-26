ef03797c9a67b7e07f3b72129a8f9d66d374f1a9
MCesarczyk <michal.cesarczyk@gmail.com>
2021-12-26 04:38:34 +0100

    Antd layout introduced

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Layout } from "antd";
+const { Content, Footer } = Layout;
 
 const Home = () => (
-    <h1>TOOLS WENT HOME</h1>
+    <Layout className="layout">
+        <Content style={{ padding: "0 50px" }}>
+            <div className="site-layout-content" style={{ margin: "100px auto" }}>
+                <h1>CNC Tools Catalog</h1>
+            </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>MCesarczyk ©2021.</Footer>
+    </Layout>
 );
 
 export default Home;
