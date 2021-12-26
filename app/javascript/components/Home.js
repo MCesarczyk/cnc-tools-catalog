b4d13346a894c19f43d382b3677a63804040a865
MCesarczyk <michal.cesarczyk@gmail.com>
2021-12-26 04:43:25 +0100

    add header

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Layout } from "antd";
+import Header from "./Header";
 const { Content, Footer } = Layout;
 
 const Home = () => (
     <Layout className="layout">
+        <Header />
         <Content style={{ padding: "0 50px" }}>
             <div className="site-layout-content" style={{ margin: "100px auto" }}>
                 <h1>CNC Tools Catalog</h1>
