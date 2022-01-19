839981663fc8298ca65501465bc13fae9722a7a6
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-01-19 13:44:54 +0100

    DRYing list building functions

--- app/javascript/assets/utils/buildArticleTable.js
+++ app/javascript/assets/utils/buildArticleTable.js
@@ -1,19 +1,10 @@
+import { formatArticle } from "./formatArticle";
+
 export const buildArticleTable = (data) => {
   let articleTable = [];
 
   data.forEach(article => {
-    const formattedArticle = {
-      key: article.id,
-      id: article.id,
-      group: article.group,
-      subgroup: article.subgroup,
-      article: article.article,
-      manufacturer: article.manufacturer,
-      description: article.description,
-      catalog: article.catalog,
-      quantity: article.quantity,
-      annotations: article.annotations,
-    };
+    const formattedArticle = formatArticle(article);
 
     articleTable = [
       ...articleTable,
--- app/javascript/assets/utils/buildToolTable.js
+++ app/javascript/assets/utils/buildToolTable.js
@@ -1,19 +1,10 @@
+import { formatTool } from "./formatTool";
+
 export const buildToolTable = (data) => {
   let toolTable = [];
 
   data.forEach(tool => {
-    const formattedTool = {
-      key: tool.id,
-      id: tool.id,
-      tooltype: tool.tooltype,
-      diameter: tool.diameter,
-      length: tool.length,
-      corner_radius: tool.corner_radius,
-      flute_number: tool.flute_number,
-      flute_length: tool.flute_length,
-      machine: tool.machine,
-      number: tool.number,
-    };
+    const formattedTool = formatTool(tool);
 
     toolTable = [
       ...toolTable,
