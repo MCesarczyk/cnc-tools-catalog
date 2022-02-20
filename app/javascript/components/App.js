5c40ea5c1cd0f45e26a4e7d4b4b3c85ec2620ca9
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-02-21 00:15:39 +0100

    update temporary availability message

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Router from "../routes";
-import "antd/dist/antd.css";
+// import "antd/dist/antd.css";
 
 export default function App() {
-    return (
-        // <Router />
-        <h1>BYE BYE ANT DESIGN</h1>
-    );
+  return (
+    <div style={{
+      display: 'grid', placeItems: 'center', minHeight: '100vh'
+    }} >
+      <div style={{textAlign: 'center'}}>
+        <h1>APPLICATION UNDER DEVELOPMENT</h1>
+        <h2>WILL BE AVAILABLE SOON!</h2>
+        <div style={{ marginTop: '4rem' }} >
+          &copy;2022 <a href="https://home.cesarczyk.dev" target="_blank" rel="noopener noreferrer">home.cesarczyk.dev</a>
+        </div>
+      </div>
+    </div >
+  );
 }
